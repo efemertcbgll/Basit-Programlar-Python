721391682359f32ec931c98fd97a0b633794c22e
Efe <53237043+efemertcbgll@users.noreply.github.com>
2021-12-03 21:11:43 +0300

    Update Girilen Günlük Maaşdan Haftalık-Aylık-Yıllık Maaş Hesaplama.py

--- Girilen Günlük Maaşdan Haftalık-Aylık-Yıllık Maaş Hesaplama.py
+++ Girilen Günlük Maaşdan Haftalık-Aylık-Yıllık Maaş Hesaplama.py
@@ -1,11 +1,11 @@
-a=input("Adini Gir: ")
-maas=int(input("Maas: "))
+a=input("Adınızı Girin: ")
+maas=int(input("Günlük Maaşınızı Girin: "))
 
 h_maas=maas*7
 a_maas=maas*30
 y_maas=maas*365
 
-print("Kanka",a)
-print("haftalik maasin=",h_maas)
-print("aylik maasin=",a_maas)
-print("yillik maasin=",y_maas)
+print("Sayın",a)
+print("Haftalık Maaşınız=",h_maas)
+print("Aylık Maaşınız=",a_maas)
+print("Yıllık Maaşınız=",y_maas)
